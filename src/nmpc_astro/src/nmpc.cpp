c15f4f8cb0613336f33ea99d9339efaa3f3dcfc9
ginkbel <jurica.vuckovic22@gmail.com>
2024-06-26 13:33:54 +0200

    revert na prije

--- src/nmpc_astro/src/nmpc.cpp
+++ src/nmpc_astro/src/nmpc.cpp
@@ -147,21 +147,21 @@ namespace nmpc_controller
             double y_dot_dot = -b * 2 * sin(2*t) * omega * omega;
 
             // Angle theta calculation assumes simple kinematic model where theta is the direction of velocity
-            // double theta = unwrap(theta_prev, atan2(y_dot, x_dot));
-            double theta = tan2(y_dot, x_dot);
+            double theta = unwrap(theta_prev, atan2(y_dot, x_dot));
+            // double theta = atan2(y_dot, x_dot);
      
             // Angular velocity (theta_dot) can be derived from cross derivative of velocity components
             double theta_dot = (x_dot * y_dot_dot - y_dot * x_dot_dot) / (x_dot * x_dot + y_dot * y_dot);
             
             x_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({x, y, theta});
-            // u_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({sqrt(x_dot*x_dot + y_dot*y_dot), theta_dot});
+            u_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({sqrt(x_dot*x_dot + y_dot*y_dot), theta_dot});
 
             normalized_time += dt_;
             theta_prev = theta;
         }
 
         opti_.set_value(_x_ref, x_ref_matrix);
-        // opti_.set_value(_u_ref, u_ref_matrix);
+        opti_.set_value(_u_ref, u_ref_matrix);
     }
 
     void NMPCController::setReferenceTrajectoryCircular(double normalized_time) {
@@ -190,21 +190,20 @@ namespace nmpc_controller
             double y_dot_dot = -R * omega * omega * sin(t);
 
             // Angle theta calculation assumes simple kinematic model where theta is the direction of velocity
-            // double theta = unwrap(theta_prev, atan2(y_dot, x_dot));
-            double theta = tan2(y_dot, x_dot);
+            double theta = unwrap(theta_prev, atan2(y_dot, x_dot));
 
             // Angular velocity (theta_dot) can be derived from cross derivative of velocity components
             double theta_dot = (x_dot * y_dot_dot - y_dot * x_dot_dot) / (x_dot * x_dot + y_dot * y_dot);
             
             x_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({x, y, theta});
-            // u_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({sqrt(x_dot*x_dot + y_dot*y_dot), theta_dot});
+            u_ref_matrix(casadi::Slice(), i) = casadi::DM::vertcat({sqrt(x_dot*x_dot + y_dot*y_dot), theta_dot});
 
             normalized_time += dt_;
             theta_prev = theta;
         }
 
         opti_.set_value(_x_ref, x_ref_matrix);
-        // opti_.set_value(_u_ref, u_ref_matrix);
+        opti_.set_value(_u_ref, u_ref_matrix);
     }
 }
 
